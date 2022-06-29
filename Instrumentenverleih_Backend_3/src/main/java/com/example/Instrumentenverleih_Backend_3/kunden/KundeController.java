b7cd53d1f492d37ae4b289f1d40870f497aaf1da
Vincent401 <vincent.fritz1201@gmail.com>
2022-06-29 02:24:06 +0200

    Das Problem mit der verbindung ins Frontend gefixt

--- Instrumentenverleih_Backend_3/src/main/java/com/example/Instrumentenverleih_Backend_3/kunden/KundeController.java
+++ Instrumentenverleih_Backend_3/src/main/java/com/example/Instrumentenverleih_Backend_3/kunden/KundeController.java
@@ -17,21 +17,24 @@ public class KundeController {
     private EmailSenderService senderService;
 
     @PostMapping("")
+    @CrossOrigin("http://localhost:4200")
     public void createKunde(@RequestBody kunde kunde){
         kundeRepository.save(kunde);
-        sendMail(kunde.getEmail(), kunde.getName());
+        sendMail(kunde.getEmail(), kunde.getName(), kunde.getInstrument());
     }
 
-    public void sendMail(String toEmail, String Name){
+    public void sendMail(String toEmail, String Name, String Instrument){
         senderService.sendEmail(toEmail,
                 "Vielen dank für ihre Bestellung",
                 "Hallo " + Name +", \n \n" +
                         "vielen dank, dass sie sich für ein Instrument von uns entschieden haben. \n" +
+                        "Sie haben bestellt: " + Instrument + "\n" +
                         "Ihr Instrument wird schnellstmöglich vorbereitet und sollte sie innerhalb von wenigen Werktagen erreichen. \n" +
                         "\n Liebe Grüße ihr \nInstrumentenverleih Mannheim");
     }
 
     @GetMapping("/{kundenId}")
+    @CrossOrigin("http://localhost:9090")
     public kunde readKunde(@PathVariable Long kundenId){
         Optional<kunde> kunde = kundeRepository.findById(kundenId);
         if (kunde.isPresent()){
